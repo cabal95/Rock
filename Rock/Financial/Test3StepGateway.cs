4f3dfb087cde032522b84938458af00df7f3c26f
Daniel Hazelbaker <daniel@blueboxmoon.com>
2017-11-28 09:24:14 -0800

    - Hackish implementation of a 3-step gateway for testing purposes.

---
+++ Rock/Financial/Test3StepGateway.cs
@@ -0,0 +1,305 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Data;
+using System.Linq;
+
+using Rock.Attribute;
+using Rock.Financial;
+using Rock.Model;
+using Rock.Web.Cache;
+
+namespace Rock.Financial
+{
+    [Description( "Test 3-Step Payment Gateway" )]
+    [Export( typeof( GatewayComponent ) )]
+    [ExportMetadata( "ComponentName", "Test3StepGateway" )]
+
+    public class Test3StepGateway : ThreeStepGatewayComponent
+    {
+
+        #region Gateway Component Implementation
+
+        /// <summary>
+        /// Gets the supported payment schedules.
+        /// </summary>
+        /// <value>
+        /// The supported payment schedules.
+        /// </value>
+        public override List<DefinedValueCache> SupportedPaymentSchedules
+        {
+            get
+            {
+                var values = new List<DefinedValueCache>();
+                values.Add( DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.TRANSACTION_FREQUENCY_ONE_TIME ) );
+                values.Add( DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.TRANSACTION_FREQUENCY_WEEKLY ) );
+                values.Add( DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.TRANSACTION_FREQUENCY_BIWEEKLY ) );
+                values.Add( DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.TRANSACTION_FREQUENCY_TWICEMONTHLY ) );
+                values.Add( DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.TRANSACTION_FREQUENCY_MONTHLY ) );
+                return values;
+            }
+        }
+
+        public override string Step2FormUrl
+        {
+            get
+            {
+                return string.Format( "~/NMIGatewayStep2.html?timestamp={0}", RockDateTime.Now.Ticks );
+            }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the gateway requires the name on card for CC processing
+        /// </summary>
+        /// <param name="financialGateway">The financial gateway.</param>
+        /// <returns></returns>
+        /// <value>
+        ///   <c>true</c> if [name on card required]; otherwise, <c>false</c>.
+        /// </value>
+        public override bool PromptForNameOnCard( FinancialGateway financialGateway )
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether [address required].
+        /// </summary>
+        /// <param name="financialGateway">The financial gateway.</param>
+        /// <returns></returns>
+        /// <value>
+        ///   <c>true</c> if [address required]; otherwise, <c>false</c>.
+        /// </value>
+        public override bool PromptForBillingAddress( FinancialGateway financialGateway )
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Charges the specified payment info.
+        /// </summary>
+        /// <param name="financialGateway">The financial gateway.</param>
+        /// <param name="paymentInfo">The payment info.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        public override FinancialTransaction Charge( FinancialGateway financialGateway, PaymentInfo paymentInfo, out string errorMessage )
+        {
+            errorMessage = string.Empty;
+
+            CreditCardPaymentInfo ccPayment = paymentInfo as CreditCardPaymentInfo;
+            if ( ccPayment != null )
+            {
+                if ( ccPayment.Code == "911" )
+                {
+                    errorMessage = "Error processing Credit Card!";
+                    return null;
+                }
+            }
+
+            var transaction = new FinancialTransaction();
+            transaction.TransactionCode = "T" + RockDateTime.Now.ToString( "yyyyMMddHHmmssFFF" );
+            return transaction;
+        }
+
+        /// <summary>
+        /// Credits the specified transaction.
+        /// </summary>
+        /// <param name="transaction">The transaction.</param>
+        /// <param name="amount">The amount.</param>
+        /// <param name="comment">The comment.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        public override FinancialTransaction Credit( FinancialTransaction transaction, decimal amount, string comment, out string errorMessage )
+        {
+            errorMessage = string.Empty;
+
+            var refundTransaction = new FinancialTransaction();
+            refundTransaction.TransactionCode = "T" + RockDateTime.Now.ToString( "yyyyMMddHHmmssFFF" );
+            return transaction;
+        }
+
+        /// <summary>
+        /// Adds the scheduled payment.
+        /// </summary>
+        /// <param name="financialGateway">The financial gateway.</param>
+        /// <param name="schedule">The schedule.</param>
+        /// <param name="paymentInfo">The payment info.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        public override FinancialScheduledTransaction AddScheduledPayment( FinancialGateway financialGateway, PaymentSchedule schedule, PaymentInfo paymentInfo, out string errorMessage )
+        {
+            errorMessage = string.Empty;
+
+            var scheduledTransaction = new FinancialScheduledTransaction();
+            scheduledTransaction.IsActive = true;
+            scheduledTransaction.StartDate = schedule.StartDate;
+            scheduledTransaction.NextPaymentDate = schedule.StartDate;
+            scheduledTransaction.TransactionCode = "T" + RockDateTime.Now.ToString( "yyyyMMddHHmmssFFF" );
+            scheduledTransaction.GatewayScheduleId = "P" + RockDateTime.Now.ToString( "yyyyMMddHHmmssFFF" );
+            scheduledTransaction.LastStatusUpdateDateTime = RockDateTime.Now;
+            return scheduledTransaction;
+        }
+
+        /// <summary>
+        /// Reactivates the scheduled payment.
+        /// </summary>
+        /// <param name="transaction">The transaction.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        public override bool ReactivateScheduledPayment( FinancialScheduledTransaction transaction, out string errorMessage )
+        {
+            transaction.IsActive = true;
+            errorMessage = string.Empty;
+            return true;
+        }
+
+        /// <summary>
+        /// Updates the scheduled payment.
+        /// </summary>
+        /// <param name="transaction">The transaction.</param>
+        /// <param name="paymentInfo">The payment info.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        public override bool UpdateScheduledPayment( FinancialScheduledTransaction transaction, PaymentInfo paymentInfo, out string errorMessage )
+        {
+            errorMessage = string.Empty;
+            return true;
+        }
+
+        /// <summary>
+        /// Cancels the scheduled payment.
+        /// </summary>
+        /// <param name="transaction">The transaction.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        public override bool CancelScheduledPayment( FinancialScheduledTransaction transaction, out string errorMessage )
+        {
+            transaction.IsActive = false;
+            errorMessage = string.Empty;
+            return true;
+        }
+
+        /// <summary>
+        /// Gets the scheduled payment status.
+        /// </summary>
+        /// <param name="transaction">The transaction.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        public override bool GetScheduledPaymentStatus( FinancialScheduledTransaction transaction, out string errorMessage )
+        {
+            transaction.LastStatusUpdateDateTime = RockDateTime.Now;
+            errorMessage = string.Empty;
+            return true;
+        }
+
+        /// <summary>
+        /// Gets the payments that have been processed for any scheduled transactions
+        /// </summary>
+        /// <param name="financialGateway">The financial gateway.</param>
+        /// <param name="startDate">The start date.</param>
+        /// <param name="endDate">The end date.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        public override List<Payment> GetPayments( FinancialGateway financialGateway, DateTime startDate, DateTime endDate, out string errorMessage )
+        {
+            errorMessage = string.Empty;
+            return new List<Payment>();
+        }
+
+        /// <summary>
+        /// Gets an optional reference identifier needed to process future transaction from saved account.
+        /// </summary>
+        /// <param name="transaction">The transaction.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        public override string GetReferenceNumber( FinancialTransaction transaction, out string errorMessage )
+        {
+            errorMessage = string.Empty;
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Gets an optional reference identifier needed to process future transaction from saved account.
+        /// </summary>
+        /// <param name="scheduledTransaction">The scheduled transaction.</param>
+        /// <param name="errorMessage">The error message.</param>
+        /// <returns></returns>
+        public override string GetReferenceNumber( FinancialScheduledTransaction scheduledTransaction, out string errorMessage )
+        {
+            errorMessage = string.Empty;
+            return string.Empty;
+        }
+
+        public override Dictionary<string, string> GetStep1Parameters( string redirectUrl )
+        {
+            var parameters = new Dictionary<string, string>();
+            parameters.Add( "redirect-url", redirectUrl );
+            return parameters;
+        }
+
+        public override string ChargeStep1( FinancialGateway financialGateway, PaymentInfo paymentInfo, out string errorMessage )
+        {
+            errorMessage = null;
+            return string.Format( "/GatewayStep2Return.aspx?token-id=1234" );
+        }
+
+        public override FinancialTransaction ChargeStep3( FinancialGateway financialGateway, string resultQueryString, out string errorMessage )
+        {
+            var transaction = new FinancialTransaction();
+            transaction.TransactionCode = Guid.NewGuid().ToString();
+            transaction.FinancialPaymentDetail = new FinancialPaymentDetail();
+
+            string ccNumber = "4444444444444444";
+            if ( !string.IsNullOrWhiteSpace( ccNumber ) )
+            {
+                // cc payment
+                var curType = DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.CURRENCY_TYPE_CREDIT_CARD );
+                transaction.FinancialPaymentDetail.CurrencyTypeValueId = curType != null ? curType.Id : ( int? ) null;
+                transaction.FinancialPaymentDetail.CreditCardTypeValueId = CreditCardPaymentInfo.GetCreditCardType( ccNumber.Replace( '*', '1' ).AsNumeric() )?.Id;
+                transaction.FinancialPaymentDetail.AccountNumberMasked = ccNumber.Masked( true );
+            }
+            else
+            {
+                // ach payment
+                var curType = DefinedValueCache.Read( Rock.SystemGuid.DefinedValue.CURRENCY_TYPE_ACH );
+                transaction.FinancialPaymentDetail.CurrencyTypeValueId = curType != null ? curType.Id : ( int? ) null;
+                transaction.FinancialPaymentDetail.AccountNumberMasked = "123456789".Masked( true );
+            }
+
+            transaction.AdditionalLavaFields = new Dictionary<string, object>();
+
+            errorMessage = null;
+
+            return transaction;
+        }
+
+        public override string AddScheduledPaymentStep1( FinancialGateway financialGateway, PaymentSchedule schedule, PaymentInfo paymentInfo, out string errorMessage )
+        {
+            throw new NotImplementedException();
+        }
+
+        public override FinancialScheduledTransaction AddScheduledPaymentStep3( FinancialGateway financialGateway, string resultQueryString, out string errorMessage )
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+    }
+}
