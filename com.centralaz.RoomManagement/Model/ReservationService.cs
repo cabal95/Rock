1cb1884b40eae1dc684631d82ce50c60696d49ad
nairdo <nairdo@gmail.com>
2018-07-27 20:51:08 -0700

    + [RM] Fixed a bug with incorrect resource quantity showing in the resource picker (Fixes #73).

--- com.centralaz.RoomManagement/Model/ReservationService.cs
+++ com.centralaz.RoomManagement/Model/ReservationService.cs
@@ -576,22 +576,28 @@ namespace com.centralaz.RoomManagement.Model
         #region Resource Conflict Methods
 
         /// <summary>
-        /// Gets the available resource quantity.
+        /// Gets the available resource quantity for the given resource, during the time (schedule)
+        /// of the given resource -- but excluding the ones used by the given resource.
         /// </summary>
         /// <param name="resource">The resource.</param>
         /// <param name="reservation">The reservation.</param>
-        /// <returns></returns>
+        /// <returns>a quantity of available resources at </returns>
         public int GetAvailableResourceQuantity( Resource resource, Reservation reservation )
         {
             // For each new reservation summary, make sure that the quantities of existing summaries that come into contact with it
             // do not exceed the resource's quantity
-            var newReservationResourceIds = reservation.ReservationResources.Select( rl => rl.ResourceId ).ToList();
 
-            var currentReservationSummaries = GetReservationSummaries( Queryable().AsNoTracking().Where( r => r.Id != reservation.Id && r.ApprovalState != ReservationApprovalState.Denied && r.ReservationResources.Any( rr => newReservationResourceIds.Contains( rr.ResourceId ) ) ), RockDateTime.Now.AddMonths( -1 ), RockDateTime.Now.AddYears( 1 ) );
+            // Get all existing non-denied reservations (for a huge time period; a month before now and a year after
+            // now) which have the given resource in them.
+            var existingReservationSummaries = GetReservationSummaries(
+                Queryable().AsNoTracking()
+                .Where( r => r.Id != reservation.Id && r.ApprovalState != ReservationApprovalState.Denied && r.ReservationResources.Any( rr => resource.Id == rr.ResourceId ) ),
+                RockDateTime.Now.AddMonths( -1 ), RockDateTime.Now.AddYears( 1 ) );
 
+            // Now narrow the reservations down to only the ones in the matching/overlapping time frame
             var reservedQuantities = GetReservationSummaries( new List<Reservation>() { reservation }.AsQueryable(), RockDateTime.Now.AddMonths( -1 ), RockDateTime.Now.AddYears( 1 ) )
                 .Select( newReservationSummary =>
-                    currentReservationSummaries.Where( currentReservationSummary =>
+                    existingReservationSummaries.Where( currentReservationSummary =>
                      ( currentReservationSummary.ReservationStartDateTime > newReservationSummary.ReservationStartDateTime || currentReservationSummary.ReservationEndDateTime > newReservationSummary.ReservationStartDateTime ) &&
                      ( currentReservationSummary.ReservationStartDateTime < newReservationSummary.ReservationEndDateTime || currentReservationSummary.ReservationEndDateTime < newReservationSummary.ReservationEndDateTime )
                     )
